46b7355cb4280f12bbbead0fa468833cde440102
vaskozlov <vaskozlov3940@gmail.com>
2023-10-13 18:32:48 +0300

    Add StaticFlatmap implementation and tests
    This commit introduces `StaticFlatmap`, a static-size, key-value container. The new implementation offers constant size and constant time operations for the flatmap operation. The file `include/isl/flatmap.hpp` was added where the template class definition of `StaticFlatmap` was made giving the key-value pair a static size array storage with constant time operations such as insert, find, at, and others.
    
    In addition to the implementation, tests were added to validate the newly introduced StaticFlatmap's functionality. The test files, `catch/isl/flatmap/indexing.cpp` and `catch/isl/flatmap/overflow.cpp`, were added where the functionality of indexing and managing overflows of StaticFlatmap were tested.
    
    A dependency on range-v3 has also been added in the `vcpkg.json` file as it is used in `include/isl/flatmap.hpp`.

---
+++ catch/isl/flatmap/overflow.cpp
@@ -0,0 +1,26 @@
+#include <isl/detail/debug/debug.hpp>
+#include <isl/flatmap.hpp>
+
+using namespace isl;
+using namespace std::string_view_literals;
+
+// NOLINTBEGIN
+
+TEST_CASE("FlatmapOverflow", "[Flatmap]")
+{
+    auto flatmap = StaticFlatmap<int, int, 10>{{10,  20},
+                                               {20,  30},
+                                               {30,  40},
+                                               {30,  40},
+                                               {40,  50},
+                                               {60,  70},
+                                               {70,  80},
+                                               {80,  80},
+                                               {90,  100},
+                                               {100, 110}};
+
+    REQUIRE(flatmap.size() == flatmap.capacity());
+    REQUIRE_THROWS_AS(flatmap.tryEmplace(110, 120), std::out_of_range);
+}
+
+// NOLINTEND
---
+++ include/isl/flatmap.hpp
@@ -0,0 +1,129 @@
+#ifndef ISL_PROJECT_FLATMAP_HPP
+#define ISL_PROJECT_FLATMAP_HPP
+
+#include <isl/isl.hpp>
+#include <isl/iterator.hpp>
+#include <range/v3/range.hpp>
+#include <range/v3/view.hpp>
+#include <utility>
+
+namespace isl {
+    template<typename Key, typename Value, size_t Size, typename Pred = std::equal_to<>>
+    class StaticFlatmap : public AutoIteratorMethods<StaticFlatmap<Key, Value, Size, Pred>> {
+    public:
+        using key_type = Key;
+        using mapped_type = Value;
+        using value_type = Pair<Key, Value>;
+        using storage_t = std::array<value_type, Size>;
+        using iterator = typename storage_t::iterator;
+        using const_iterator = typename storage_t::const_iterator;
+
+    private:
+        storage_t storage{};
+        size_t occupied{};
+
+    public:
+        ISL_DECL auto size() const noexcept -> size_t {
+            return occupied;
+        }
+
+        ISL_DECL static auto capacity() noexcept -> size_t {
+            return Size;
+        }
+
+        ISL_DECL auto begin() noexcept -> iterator {
+            return storage.begin();
+        }
+
+        ISL_DECL auto end() noexcept -> iterator {
+            return storage.begin() + occupied;
+        }
+
+        ISL_DECL auto begin() const noexcept -> const_iterator {
+            return storage.begin();
+        }
+
+        ISL_DECL auto end() const noexcept -> const_iterator {
+            return storage.begin() + occupied;
+        }
+
+        template<typename K, typename... Ts>
+        constexpr auto tryEmplace(K &&key,
+                                  Ts &&...args) -> Pair<iterator, bool>requires std::constructible_from<value_type, Key, Ts...> {
+            if (occupied == capacity()) {
+                throw std::out_of_range("StaticFlatmap is full. Unable to emplace.");
+            }
+
+            auto result = find(key);
+
+            if (result != end()) {
+                return {result, false};
+            }
+
+            storage[occupied++] = value_type(std::forward<K>(key), std::forward<Ts>(args)...);
+            return {result, true};
+        }
+
+        ISL_DECL auto at(const Key &key) ISL_LIFETIMEBOUND -> Value & {
+            return staticAt(*this, key);
+        }
+
+        ISL_DECL auto at(const Key &key) const ISL_LIFETIMEBOUND -> const Value & {
+            return staticAt(*this, key);
+        }
+
+        ISL_DECL auto operator[](const Key &key) ISL_LIFETIMEBOUND -> Value & {
+            return tryEmplace(key).first->second;
+        }
+
+        ISL_DECL auto operator[](const Key &key) const ISL_LIFETIMEBOUND -> const Value & {
+            return at(key);
+        }
+
+        ISL_DECL auto contains(const Key &key) const noexcept -> bool {
+            return find(key) != end();
+        }
+
+        ISL_DECL auto find(const Key &key) noexcept -> iterator {
+            return staticFind(*this, key);
+        }
+
+        ISL_DECL auto find(const Key &key) const noexcept -> const_iterator {
+            return staticFind(*this, key);
+        }
+
+        StaticFlatmap() = default;
+
+        constexpr StaticFlatmap(const std::initializer_list<value_type> &initial_data) {
+            for (const value_type &value: initial_data) {
+                if (occupied == capacity()) {
+                    throw std::runtime_error{
+                            "StaticFlatmap capacity limit reached during initialization"};
+                }
+
+                storage[occupied++] = value;
+            }
+        }
+
+    private:
+        template<typename Self>
+        ISL_DECL static auto staticFind(Self &self, const Key &key) noexcept -> auto {
+            return std::find_if(std::begin(self), std::end(self), [&key](const value_type &value) {
+                return Pred{}(key, value.first);
+            });
+        }
+
+        template<typename Self>
+        ISL_DECL static auto staticAt(Self &self, const Key &key) -> auto & {
+            auto elem = self.find(key);
+
+            if (elem == self.end()) {
+                throw std::out_of_range("Key not found in StaticFlatmap at() method.");
+            }
+
+            return elem->second;
+        }
+    };
+}// namespace isl
+
+#endif /* ISL_PROJECT_FLATMAP_HPP */
