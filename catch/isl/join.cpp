12f6f44a3a95034c1ea9242644aa1048df3e15f2
vaskozlov <vaskozlov3940@gmail.com>
2023-10-13 18:33:05 +0300

    join function

---
+++ catch/isl/join.cpp
@@ -0,0 +1,21 @@
+#include <isl/detail/debug/debug.hpp>
+#include <isl/join.hpp>
+
+using namespace isl;
+using namespace string_view_literals;
+using namespace std::string_view_literals;
+
+TEST_CASE("JoinInts", "[Join]")
+{
+    DEBUG_VAR vector_of_int = std::vector<int>{10, 20, 30};// NOLINT
+
+    REQUIRE(
+            isl::join(
+                    vector_of_int,
+                    [](auto &&elem) {
+                        return fmt::to_string(elem);
+                    },
+                    ", ") == "10, 20, 30");
+
+    REQUIRE(isl::join(vector_of_int, ", ") == "10, 20, 30");
+}
