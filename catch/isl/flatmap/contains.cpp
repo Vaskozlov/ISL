bf7425c209118d0f93904c4322716529f9728b92
vaskozlov <vaskozlov3940@gmail.com>
2023-10-13 18:33:33 +0300

    test for flatmap

---
+++ catch/isl/flatmap/contains.cpp
@@ -0,0 +1,22 @@
+#include <isl/detail/debug/debug.hpp>
+#include <isl/flatmap.hpp>
+
+using namespace isl;
+
+TEST_CASE("FlatmapContains", "[Flatmap]")
+{
+    static constexpr auto flatmap =
+            StaticFlatmap<int, int, 10>{{10, 20},
+                                        {20, 30},
+                                        {30, 40}};// NOLINT
+
+    REQUIRE(flatmap.size() == 3);
+    REQUIRE(flatmap.capacity() == 10);
+
+    REQUIRE(flatmap.contains(10));
+    REQUIRE(flatmap.contains(20));
+    REQUIRE(flatmap.contains(30));
+
+    REQUIRE(!flatmap.contains(0));
+    REQUIRE(!flatmap.contains(40));
+}
