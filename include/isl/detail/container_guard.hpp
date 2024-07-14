8fa78f1f35fbe2fefd732d6b9f3edfc4df419d7b
vaskozlov <vaskozlov3940@gmail.com>
2024-07-14 15:35:20 +0500

    Revert "fix: comparison operator for container guard"
    This reverts commit cc320a139b75ec2c05255dc06fff98f8696bf8bf.

--- include/isl/detail/container_guard.hpp
+++ include/isl/detail/container_guard.hpp
@@ -1,10 +1,7 @@
 #ifndef ISL_PROJECT_CONTAINER_GUARD_HPP
 #define ISL_PROJECT_CONTAINER_GUARD_HPP
 
-#include <algorithm>
-#include <concepts>
 #include <isl/detail/defines.hpp>
-#include <ranges>
 #include <utility>
 
 namespace isl::detail
@@ -25,18 +22,6 @@ namespace isl::detail
         explicit ContainerGuard(R &&range)
           : T{range.begin(), range.end()}
         {}
-
-
-        [[nodiscard]] auto operator==(const ContainerGuard<T> &other) const noexcept -> bool
-        {
-            return static_cast<const T &>(*this) == static_cast<const T &>(other);
-        }
-
-        [[nodiscard]] auto
-            operator<=>(const ContainerGuard<T> &other) const noexcept -> decltype(auto)
-        {
-            return static_cast<const T &>(*this) <=> static_cast<const T &>(other);
-        }
     };
 }// namespace isl::detail
 
