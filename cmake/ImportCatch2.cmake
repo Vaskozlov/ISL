4588450ed406dd7eb2f4999735f48c6016a45099
vaskozlov <vaskozlov3940@gmail.com>
2024-07-16 13:43:31 +0500

    fix: exclude catch2 from project

--- cmake/ImportCatch2.cmake
+++ cmake/ImportCatch2.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
         Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG devel
+        EXCLUDE_FROM_ALL
 )
 
 FetchContent_MakeAvailable(Catch2)
