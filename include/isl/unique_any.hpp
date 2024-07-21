785976292e2a3b41736dfaa9405a59722f5cc0cb
vaskozlov <vaskozlov3940@gmail.com>
2024-07-21 15:41:52 +0500

    feat: adding constexpr to unique_any

--- include/isl/unique_any.hpp
+++ include/isl/unique_any.hpp
@@ -19,7 +19,7 @@ namespace isl
 
     public:
         template<typename T>
-        explicit UniqueAny(T &&object)
+        ISL_DECL UniqueAny(T &&object)
         {
             emplace<T>(std::forward<T>(object));
         }
@@ -33,7 +33,7 @@ namespace isl
 
         auto operator=(const UniqueAny &) -> void = delete;
 
-        auto operator=(UniqueAny &&other) noexcept -> UniqueAny &
+        constexpr auto operator=(UniqueAny &&other) noexcept -> UniqueAny &
         {
             std::swap(pointer, other.pointer);
             std::swap(deleter, other.deleter);
@@ -42,29 +42,28 @@ namespace isl
             return *this;
         }
 
-        [[nodiscard]] auto hasValue() const noexcept -> bool
+        ISL_DECL auto hasValue() const noexcept -> bool
         {
             return pointer != nullptr;
         }
 
-        [[nodiscard]] auto getTypeIndex() const noexcept -> std::type_index
+        ISL_DECL auto getTypeIndex() const noexcept -> std::type_index
         {
             return typeIndex;
         }
 
         template<typename T, typename... Ts>
-        auto emplace(Ts &&...args) -> void
+        constexpr auto emplace(Ts &&...args) -> void
         {
             pointer = static_cast<void *>(new T{std::forward<Ts>(args)...});
             typeIndex = std::type_index{typeid(T)};
-
             deleter = [](void *p) {
                 delete static_cast<T *>(p);
             };
         }
 
         template<typename T>
-        [[nodiscard]] auto get() -> isl::UniquePtr<T>
+        ISL_DECL auto get() -> isl::UniquePtr<T>
         {
             if (std::type_index{typeid(T)} == typeIndex) {
                 deleter = nullptr;
@@ -76,7 +75,7 @@ namespace isl
         }
 
         template<typename T>
-        [[nodiscard]] auto getAsBaseClass() -> isl::UniquePtr<T>
+        ISL_DECL auto getAsBaseClass() -> isl::UniquePtr<T>
         {
             if (auto *casted_ptr = dynamic_cast<T *>(pointer); casted_ptr != nullptr) {
                 deleter = nullptr;
@@ -87,7 +86,7 @@ namespace isl
             return nullptr;
         }
 
-        ~UniqueAny()
+        constexpr ~UniqueAny()
         {
             if (deleter != nullptr) {
                 deleter(pointer);
