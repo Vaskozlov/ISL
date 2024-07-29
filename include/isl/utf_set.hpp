7b72bfdfbd2dda2436ddd488d19ee713cb7b7aa6
vaskozlov <vaskozlov3940@gmail.com>
2024-07-29 13:08:38 +0500

    feat: source part of isl library

--- include/isl/utf_set.hpp
+++ include/isl/utf_set.hpp
@@ -23,63 +23,25 @@ namespace isl
             std::bitset<asciiStorageSize>
                 ascii_symbols,
             std::unordered_set<char32_t>
-                non_ascii_symbols)
-          : asciiSymbolsStorage{ascii_symbols}
-          , nonAsciiStorage{std::move(non_ascii_symbols)}
-        {}
+                non_ascii_symbols);
 
         UtfSet(
             std::bitset<asciiStorageSize>
                 ascii_symbols,
-            const std::vector<Range<char32_t>> &ranges)
-          : asciiSymbolsStorage{ascii_symbols}
-        {
-            for (const auto range : ranges) {
-                set(range, true);
-            }
-        }
-
+            const std::vector<Range<char32_t>> &ranges);
 
         [[nodiscard]] auto empty() const noexcept -> bool
         {
             return asciiSymbolsStorage.none() && nonAsciiStorage.empty();
         }
 
-        [[nodiscard]] auto at(char32_t chr) const noexcept -> bool
-        {
-            if (chr < asciiStorageSize) [[likely]] {
-                return asciiSymbolsStorage.test(chr);
-            }
+        [[nodiscard]] auto at(char32_t chr) const noexcept -> bool;
 
-            return nonAsciiStorage.contains(chr);
-        }
-
-        auto set(char32_t chr, bool value = true) -> void
-        {
-            if (chr < asciiStorageSize) [[likely]] {
-                asciiSymbolsStorage.set(chr, value);
-            } else [[unlikely]] {
-                setBigChar(chr, value);
-            }
-        }
-
-        auto set(Range<char32_t> range, bool value = true) -> void
-        {
-            ISL_UNROLL_N(4)
-            for (const char32_t chr : range) {
-                set(chr, value);
-            }
-        }
+        auto set(char32_t chr, bool value = true) -> void;
+        auto set(Range<char32_t> range, bool value = true) -> void;
 
     private:
-        auto setBigChar(char32_t chr, bool value) -> void
-        {
-            if (value) [[likely]] {
-                nonAsciiStorage.insert(chr);
-            } else [[unlikely]] {
-                nonAsciiStorage.erase(chr);
-            }
-        }
+        auto setBigChar(char32_t chr, bool value) -> void;
     };
 }// namespace isl
 
---
+++ src/isl/utf_set.cpp
@@ -0,0 +1,59 @@
+#include <isl/utf_set.hpp>
+
+namespace isl
+{
+    UtfSet::UtfSet(
+        std::bitset<asciiStorageSize>
+            ascii_symbols,
+        std::unordered_set<char32_t>
+            non_ascii_symbols)
+      : asciiSymbolsStorage{ascii_symbols}
+      , nonAsciiStorage{std::move(non_ascii_symbols)}
+    {}
+
+    UtfSet::UtfSet(
+        std::bitset<asciiStorageSize>
+            ascii_symbols,
+        const std::vector<Range<char32_t>> &ranges)
+      : asciiSymbolsStorage{ascii_symbols}
+    {
+        for (const auto range : ranges) {
+            set(range, true);
+        }
+    }
+
+    auto UtfSet::at(char32_t chr) const noexcept -> bool
+    {
+        if (chr < asciiStorageSize) [[likely]] {
+            return asciiSymbolsStorage.test(chr);
+        }
+
+        return nonAsciiStorage.contains(chr);
+    }
+
+    auto UtfSet::set(char32_t chr, bool value) -> void
+    {
+        if (chr < asciiStorageSize) [[likely]] {
+            asciiSymbolsStorage.set(chr, value);
+        } else [[unlikely]] {
+            setBigChar(chr, value);
+        }
+    }
+
+    auto UtfSet::set(Range<char32_t> range, bool value) -> void
+    {
+        ISL_UNROLL_N(4)
+        for (const char32_t chr : range) {
+            set(chr, value);
+        }
+    }
+
+    auto UtfSet::setBigChar(char32_t chr, bool value) -> void
+    {
+        if (value) [[likely]] {
+            nonAsciiStorage.insert(chr);
+        } else [[unlikely]] {
+            nonAsciiStorage.erase(chr);
+        }
+    }
+}// namespace isl
