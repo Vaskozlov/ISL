f8de481f7a1974867d93bce1e753da690f1195bf
vaskozlov <vaskozlov3940@gmail.com>
2024-08-01 12:43:18 +0500

    fix: local function should be static

--- benchmark/pool.cpp
+++ benchmark/pool.cpp
@@ -4,9 +4,9 @@
 #include <random>
 #include <vector>
 
-isl::thread::Pool ThreadPool(4);
+static isl::thread::Pool ThreadPool(4);
 
-auto classicFibonacci(isl::u64 n) -> isl::u64
+static auto classicFibonacci(isl::u64 n) -> isl::u64
 {
     if (n <= 1) {
         return n;
@@ -15,15 +15,15 @@ auto classicFibonacci(isl::u64 n) -> isl::u64
     return classicFibonacci(n - 1) + classicFibonacci(n - 2);
 }
 
-auto fibonacciWithThreadPool(isl::u64 n, isl::thread::Pool &pool) -> isl::Task<isl::u64>
+static auto fibonacciWithThreadPool(isl::u64 n, isl::thread::Pool *pool) -> isl::Task<isl::u64>
 {
     if (n <= 1) {
         co_return n;
     }
 
     if (n > 20) {
-        auto first = pool.submit(fibonacciWithThreadPool(n - 1, pool));
-        auto second = pool.submit(fibonacciWithThreadPool(n - 2, pool));
+        auto first = pool->submit(fibonacciWithThreadPool(n - 1, pool));
+        auto second = pool->submit(fibonacciWithThreadPool(n - 2, pool));
 
         auto lhs = co_await first;
         auto rhs = co_await second;
@@ -33,11 +33,11 @@ auto fibonacciWithThreadPool(isl::u64 n, isl::thread::Pool &pool) -> isl::Task<i
     co_return classicFibonacci(n - 1) + classicFibonacci(n - 2);
 }
 
-auto createTestNumbers() -> std::vector<isl::u32>
+static auto createTestNumbers() -> std::vector<isl::u32>
 {
     std::vector<isl::u32> v;
 
-    for (std::size_t i = 20; i != 30; ++i) {
+    for (std::size_t i = 30; i != 36; ++i) {
         v.emplace_back(i);
     }
 
@@ -48,7 +48,7 @@ auto createTestNumbers() -> std::vector<isl::u32>
     return v;
 }
 
-void classicFibonacciBenchmark(benchmark::State &state)
+static void classicFibonacciBenchmark(benchmark::State &state)
 {
     auto numbers = createTestNumbers();
     for (auto _ : state) {
@@ -60,12 +60,12 @@ void classicFibonacciBenchmark(benchmark::State &state)
 
 BENCHMARK(classicFibonacciBenchmark);
 
-void fibonacciWithThreadPoolBenchmark(benchmark::State &state)
+static void fibonacciWithThreadPoolBenchmark(benchmark::State &state)
 {
     auto numbers = createTestNumbers();
     for (auto _ : state) {
         for (auto v : numbers) {
-            auto task = ThreadPool.submit(fibonacciWithThreadPool(v, ThreadPool));
+            auto task = ThreadPool.submit(fibonacciWithThreadPool(v, std::addressof(ThreadPool)));
             benchmark::DoNotOptimize(task.await());
         }
     }
