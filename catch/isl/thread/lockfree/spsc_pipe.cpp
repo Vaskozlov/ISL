54141c6f23756925f3f7a64eda9ff37db8207d2a
vaskozlov <vaskozlov3940@gmail.com>
2024-08-02 14:11:22 +0500

    feat!: spsc pipe

---
+++ catch/isl/thread/lockfree/spsc_pipe.cpp
@@ -0,0 +1,59 @@
+#include <chrono>
+#include <isl/detail/debug/debug.hpp>
+#include <isl/thread/lockfree/spsc_pipe.hpp>
+#include <latch>
+#include <thread>
+
+using Pipe = isl::thread::lock_free::SpscPipe<std::size_t, 4096>;
+
+static auto pipeProducer(Pipe &pipe, std::size_t count, std::latch &latch) -> void
+{
+    latch.arrive_and_wait();
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    for (std::size_t j = 0; j != 100; ++j) {
+        for (std::size_t i = 0; i != count; ++i) {
+            while (!pipe.tryEmplace(i)) {}
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    fmt::println(
+        "Produced in {}ms",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+}
+
+static auto pipeConsumer(Pipe &pipe, std::size_t count, std::latch &latch) -> void
+{
+    latch.arrive_and_wait();
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    for (std::size_t j = 0; j != 100; ++j) {
+        for (std::size_t i = 0; i != count; ++i) {
+            std::size_t expected = i;
+            std::size_t value = 0;
+
+            while (!pipe.tryPop(value)) {
+                std::this_thread::yield();
+            }
+
+            REQUIRE(value == expected);
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    fmt::println(
+        "Consumed in {}ms",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+}
+
+TEST_CASE("SpscPipe", "[LockFree]")
+{
+    auto pipe = Pipe{};
+    auto latch = std::latch(2);
+    auto producer = std::thread{pipeProducer, std::ref(pipe), 4096 * 4, std::ref(latch)};
+    auto consumer = std::thread{pipeConsumer, std::ref(pipe), 4096 * 4, std::ref(latch)};
+
+    producer.join();
+    consumer.join();
+}
---
+++ include/isl/thread/lockfree/spsc_pipe.hpp
@@ -0,0 +1,140 @@
+#ifndef ISL_PROJECT_SPSC_PIPE_HPP
+#define ISL_PROJECT_SPSC_PIPE_HPP
+
+#include <array>
+#include <atomic>
+#include <isl/isl.hpp>
+
+namespace isl::thread::lock_free
+{
+    template<typename T, std::size_t Size>
+    class SpscPipe
+    {
+    public:
+        using value_type = T;
+        using size_type = std::size_t;
+        using CursorType = std::atomic<size_type>;
+
+        static_assert(CursorType::is_always_lock_free);
+
+    private:
+        class RowBuffer
+        {
+        private:
+            alignas(T) std::array<std::byte, sizeof(T)> rowBuffer;
+
+        public:
+            [[nodiscard]] auto getAddress() noexcept -> void *
+            {
+                return static_cast<void *>(std::addressof(rowBuffer));
+            }
+
+            [[nodiscard]] auto getValuePtr() noexcept -> T *
+            {
+                return static_cast<T *>(getAddress());
+            }
+
+            template<typename... Ts>
+            auto construct(Ts &&...args) -> void
+            {
+                std::construct_at(getValuePtr(), std::forward<Ts>(args)...);
+            }
+
+            auto destroy() -> void
+            {
+                std::destroy_at(getValuePtr());
+            }
+        };
+
+        std::array<RowBuffer, Size> ring;
+
+        ISL_HARDWARE_CACHE_LINE_ALIGN CursorType pushCursor;
+
+        ISL_HARDWARE_CACHE_LINE_ALIGN size_type pushCursorCached;
+
+        ISL_HARDWARE_CACHE_LINE_ALIGN CursorType popCursor;
+
+        ISL_HARDWARE_CACHE_LINE_ALIGN size_type popCursorCached;
+
+        ISL_HARDWARE_CACHE_LINE_PADDING(size_type);
+
+        [[nodiscard]] static auto isFull(size_type push_cursor, size_t pop_cursor) noexcept -> bool
+        {
+            return (push_cursor - pop_cursor) == capacity();
+        }
+
+        [[nodiscard]] static auto isEmpty(size_type push_cursor, size_t pop_cursor) noexcept -> bool
+        {
+            return push_cursor == pop_cursor;
+        }
+
+    public:
+        [[nodiscard]] static auto capacity() noexcept -> size_type
+        {
+            return Size;
+        }
+
+        [[nodiscard]] auto contained() const noexcept -> size_type
+        {
+            auto push_cursor = pushCursor.load(std::memory_order_relaxed);
+            auto pop_cursor = popCursor.load(std::memory_order_relaxed);
+
+            return push_cursor - pop_cursor;
+        }
+
+        [[nodiscard]] auto wasEmpty() const noexcept -> bool
+        {
+            return contained() == 0;
+        }
+
+        [[nodiscard]] auto wasFull() const noexcept -> bool
+        {
+            auto push_cursor = pushCursor.load(std::memory_order_relaxed);
+            auto pop_cursor = popCursor.load(std::memory_order_relaxed);
+
+            return isFull(push_cursor, push_cursor);
+        }
+
+        template<typename... Ts>
+        auto tryEmplace(Ts &&...args) -> bool
+        {
+            auto push_cursor = pushCursor.load(std::memory_order_relaxed);
+            auto next_push_cursor = (push_cursor + 1) % Size;
+
+            if (next_push_cursor == popCursorCached) {
+                popCursorCached = popCursor.load(std::memory_order_acquire);
+
+                if (next_push_cursor == popCursorCached) {
+                    return false;
+                }
+            }
+
+            ring[push_cursor].construct(std::forward<Ts>(args)...);
+            pushCursor.store(next_push_cursor, std::memory_order_release);
+            return true;
+        }
+
+        auto tryPop(T &value) -> bool
+        {
+            auto pop_cursor = popCursor.load(std::memory_order_relaxed);
+
+            if (pop_cursor == pushCursorCached) {
+                pushCursorCached = pushCursor.load(std::memory_order_acquire);
+
+                if (pop_cursor == pushCursorCached) {
+                    return false;
+                }
+            }
+
+            value = std::move(*ring[pop_cursor].getValuePtr());
+            ring[pop_cursor].destroy();
+
+            auto next_pop_cursor = (pop_cursor + 1) % Size;
+            popCursor.store(next_pop_cursor, std::memory_order_release);
+
+            return true;
+        }
+    };
+}// namespace isl::thread::lock_free
+
+#endif /* ISL_PROJECT_SPSC_PIPE_HPP */
