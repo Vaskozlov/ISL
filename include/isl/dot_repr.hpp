ed2aea754588fe084bdaadd14bc65992454dd9e9
vaskozlov <vaskozlov3940@gmail.com>
2024-08-22 15:22:45 +0500

    fix: dot repr should not support isl::GSS anymore

--- include/isl/dot_repr.hpp
+++ include/isl/dot_repr.hpp
@@ -3,101 +3,53 @@
 
 #include <ankerl/unordered_dense.h>
 #include <functional>
-#include <isl/gss.hpp>
 #include <isl/id_generator.hpp>
 
 namespace isl::dot
 {
-    namespace detail
+    using Edge = std::pair<std::size_t, std::size_t>;
+
+    struct EdgeInfo
     {
-        using Edge = std::pair<std::size_t, std::size_t>;
+        std::string label{};
+        std::string color = "black";
+    };
 
-        struct EdgeInfo
-        {
-            std::string label{};
-            std::string color = "black";
-        };
-
-        struct NodeInfo
-        {
-            std::string label{};
-            std::string color = "black";
-        };
-
-        struct TreeInformationCommon
-        {
-            std::set<Edge> edges{};
-            ankerl::unordered_dense::map<Edge, EdgeInfo> edgesInfo{};
-            ankerl::unordered_dense::map<SmallId, NodeInfo> nodesInfo{};
-            IdGenerator<SmallId> idGenerator{1};
-
-            auto generateDotRepr() const -> std::string;
-        };
-
-        template<typename T, typename ToStrArg>
-        struct TreeInformation : public TreeInformationCommon
-        {
-            std::map<T, SmallId> nodeToId{};
-            std::function<std::string(ToStrArg)> nodeTypeToString;
-
-            auto createIdForNode(T node) -> SmallId
-            {
-                auto it = nodeToId.find(node);
-
-                if (it == nodeToId.end()) {
-                    const auto node_id = idGenerator.next();
-                    nodeToId.emplace(std::move(node), node_id);
-                    return node_id;
-                }
-
-                return it->second;
-            }
-        };
-
-        template<typename T>
-        auto createDotRepresentation(
-            GSSNode<T>
-                node,
-            TreeInformation<GSSNode<T>, T> &tree_information) -> void
-        {
-            auto node_id = tree_information.createIdForNode(node);
-            tree_information.nodesInfo.try_emplace(
-                node_id,
-                NodeInfo{
-                    .label = tree_information.nodeTypeToString(node->value),
-                });
-
-            for (auto &prev : node->previous) {
-                auto prev_id = tree_information.createIdForNode(prev);
-                auto edge = Edge{node_id, prev_id};
-
-                if (!tree_information.edges.contains(edge)) {
-                    tree_information.edges.emplace(edge);
-                    tree_information.edgesInfo.emplace(edge, EdgeInfo{});
-                    createDotRepresentation(prev, tree_information);
-                }
-            }
-        }
-    }// namespace detail
-
-    template<typename T>
-    auto createDotRepresentation(
-        const GSStack<T> &stack,
-        std::function<std::string(T)>
-            to_string_function) -> std::string
+    struct NodeInfo
     {
-        using namespace detail;
+        std::string label{};
+        std::string color = "black";
+    };
 
-        auto tree_information = TreeInformation<GSSNode<T>, T>{
-            .nodeTypeToString = std::move(to_string_function),
-        };
+    struct TreeInformationCommon
+    {
+        std::set<Edge> edges{};
+        ankerl::unordered_dense::map<Edge, EdgeInfo> edgesInfo{};
+        ankerl::unordered_dense::map<SmallId, NodeInfo> nodesInfo{};
+        IdGenerator<SmallId> idGenerator{1};
 
-        for (const auto &node : stack.getHead()) {
-            createDotRepresentation(node, tree_information);
+        auto generateDotRepr() const -> std::string;
+    };
+
+    template<typename T, typename ToStrArg>
+    struct TreeInformation : public TreeInformationCommon
+    {
+        std::map<T, SmallId> nodeToId{};
+        std::function<std::string(ToStrArg)> nodeTypeToString;
+
+        auto createIdForNode(T node) -> SmallId
+        {
+            auto it = nodeToId.find(node);
+
+            if (it == nodeToId.end()) {
+                const auto node_id = idGenerator.next();
+                nodeToId.emplace(std::move(node), node_id);
+                return node_id;
+            }
+
+            return it->second;
         }
-
-        return tree_information.generateDotRepr();
-    }
+    };
 }// namespace isl::dot
 
 #endif /* ISL_PROJECT_DOT_REPR_HPP */
