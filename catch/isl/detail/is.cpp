34d6c8235daffb749c01e405a5ea8c935f21963a
vaskozlov <vaskozlov3940@gmail.com>
2025-03-21 17:40:18 +0300

    is and as do not work in gcc at compile time

--- catch/isl/detail/is.cpp
+++ catch/isl/detail/is.cpp
@@ -1,4 +1,5 @@
 #include "test_structures.hpp"
+
 #include <isl/detail/debug/debug.hpp>
 #include <isl/isl.hpp>
 
@@ -7,18 +8,18 @@
 TEST_CASE("TestIsOnSimilarTypes", "[IsConversion]")
 {
     using namespace isl;
-    constexpr auto value = as<i64>(42);
+    auto value = as<i64>(42);
 
-    STATIC_REQUIRE(isl::is<i64>(value));
+    REQUIRE(isl::is<i64>(value));
 }
 
 TEST_CASE("TestIsOnPointer", "[IsConversion]")
 {
     using namespace isl;
 
-    auto b = B{};
-    auto *a = as<const A *>(&b);
-    auto *b_from_a = as<const B *>(a);
+    const auto b = B{};
+    const auto *a = as<const A *>(&b);
+    const auto *b_from_a = as<const B *>(a);
 
     REQUIRE(is<A *>(&b));
     REQUIRE(is<B *>(&b));
@@ -36,21 +37,21 @@ TEST_CASE("TestIsOnReferences", "[IsConversion]")
 {
     using namespace isl;
 
-    constexpr static auto b = B{};
+    const auto b = B{};
 
-    constexpr static auto &a = as<const A &>(b);
-    constexpr static auto &b_from_a = as<const B &>(a);
+    const auto &a = as<const A &>(b);
+    const auto &b_from_a = as<const B &>(a);
 
-    STATIC_REQUIRE(is<const A &>(b));
-    STATIC_REQUIRE(is<const B &>(b));
-    STATIC_REQUIRE(is<const A &>(a));
-    STATIC_REQUIRE(is<const B &>(a));
-    STATIC_REQUIRE(is<const A &>(b_from_a));
-    STATIC_REQUIRE(is<const B &>(b_from_a));
+    REQUIRE(is<const A &>(b));
+    REQUIRE(is<const B &>(b));
+    REQUIRE(is<const A &>(a));
+    REQUIRE(is<const B &>(a));
+    REQUIRE(is<const A &>(b_from_a));
+    REQUIRE(is<const B &>(b_from_a));
 
-    STATIC_REQUIRE_FALSE(is<C &>(a));
-    STATIC_REQUIRE_FALSE(is<C &>(b));
-    STATIC_REQUIRE_FALSE(is<C &>(b_from_a));
+    REQUIRE_FALSE(is<C &>(a));
+    REQUIRE_FALSE(is<C &>(b));
+    REQUIRE_FALSE(is<C &>(b_from_a));
 }
 
 // NOLINTEND
