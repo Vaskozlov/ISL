4e10225a48ce86ad3fc6094e4d62cbfbf2341db6
vaskozlov <vaskozlov3940@gmail.com>
2025-06-01 16:03:45 +0300

    fix: mac os clang wants explicit template parameters

--- catch/isl/linalg/spline.cpp
+++ catch/isl/linalg/spline.cpp
@@ -11,15 +11,15 @@ TEST_CASE("IslSpline", "IslLinalg")
     std::vector<float> dataY{0.0, 1.5, 0.3, 2.8, 1.1, 3.0, -5.0, 0.0, -2.0, 2.0};
 
     constexpr static std::array<CubicSplineCoefficients<float>, 9> expected_coefficients{
-        CubicSplineCoefficients{0.0F,  2.6743518808904394F, -2.220446049250e-16F, -1.1743518808904F  },
-        CubicSplineCoefficients{1.5F,  -0.84870376178087F,  -3.5230556426713F,    3.1717594044521F   },
-        CubicSplineCoefficients{0.3F,  1.6204631662330753F, 5.992222570685272F,   -5.112685736918348F},
-        CubicSplineCoefficients{2.8F,  -1.73314890315142F,  -9.34583464006977F,   9.3789835432216F   },
-        CubicSplineCoefficients{1.1F,  7.712132446372618F,  18.791115989593784F,  -49.25889110728461F},
-        CubicSplineCoefficients{3.0F,  9.317511909335975F,  -10.76421867477701F,  2.2914741738388775F},
-        CubicSplineCoefficients{-5.0F, 2.933359899275105F,  8.484164385469555F,   -6.41752428474466F },
-        CubicSplineCoefficients{0.0F,  0.64911581598023F,   -10.7684084687644F,   8.1192926527841F   },
-        CubicSplineCoefficients{-2.0F, 3.47017683680396F,   13.5894694895881F,    -9.0596463263920F  },
+        CubicSplineCoefficients<float>{0.0F,  2.67435188089F,   -2.2204460492e-16F, -1.17435188089F  },
+        CubicSplineCoefficients<float>{1.5F,  -0.848703761780F, -3.52305564267F,    3.17175940441F   },
+        CubicSplineCoefficients<float>{0.3F,  1.6204631662330F, 5.99222257068F,     -5.1126857369183F},
+        CubicSplineCoefficients<float>{2.8F,  -1.73314890315F,  -9.345834640069F,   9.37898354322F   },
+        CubicSplineCoefficients<float>{1.1F,  7.712132446372F,  18.7911159895937F,  -49.258891107284F},
+        CubicSplineCoefficients<float>{3.0F,  9.317511909335F,  -10.764218674777F,  2.29147417383887F},
+        CubicSplineCoefficients<float>{-5.0F, 2.933359899275F,  8.4841643854695F,   -6.417524284744F },
+        CubicSplineCoefficients<float>{0.0F,  0.649115815980F,  -10.76840846876F,   8.11929265278F   },
+        CubicSplineCoefficients<float>{-2.0F, 3.47017683680F,   13.58946948958F,    -9.05964632639F  },
     };
 
     constexpr static std::array<float, 10> test_points{
