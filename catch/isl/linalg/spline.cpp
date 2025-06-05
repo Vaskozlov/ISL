f8144172bc766c20120c4e49697e3351c4645ea3
vaskozlov <vaskozlov3940@gmail.com>
2025-06-05 11:08:15 +0300

    fix: improved spline test

--- catch/isl/linalg/spline.cpp
+++ catch/isl/linalg/spline.cpp
@@ -7,19 +7,87 @@ TEST_CASE("IslSpline", "IslLinalg")
 {
     using namespace isl::interpolation;
 
-    std::vector<float> dataX{0.0, 1.0, 2.0, 3.0, 4.0, 4.2, 7.0, 8.0, 9.0, 9.5};
-    std::vector<float> dataY{0.0, 1.5, 0.3, 2.8, 1.1, 3.0, -5.0, 0.0, -2.0, 2.0};
+    static constexpr std::array<float, 10> dataX{
+        0.0F,
+        1.0F,
+        2.0F,
+        3.0F,
+        4.0F,
+        4.2F,
+        7.0F,
+        8.0F,
+        9.0F,
+        9.5F,
+    };
+
+    static constexpr std::array<float, 10> dataY{
+        0.0F,
+        1.5F,
+        0.3F,
+        2.8F,
+        1.1F,
+        3.0F,
+        -5.0F,
+        0.0F,
+        -2.0F,
+        2.0F,
+    };
 
     constexpr static std::array<CubicSplineCoefficients<float>, 9> expected_coefficients{
-        CubicSplineCoefficients<float>{0.0F,  2.67435188089F,   -2.2204460492e-16F, -1.17435188089F  },
-        CubicSplineCoefficients<float>{1.5F,  -0.848703761780F, -3.52305564267F,    3.17175940441F   },
-        CubicSplineCoefficients<float>{0.3F,  1.6204631662330F, 5.99222257068F,     -5.1126857369183F},
-        CubicSplineCoefficients<float>{2.8F,  -1.73314890315F,  -9.345834640069F,   9.37898354322F   },
-        CubicSplineCoefficients<float>{1.1F,  7.712132446372F,  18.7911159895937F,  -49.258891107284F},
-        CubicSplineCoefficients<float>{3.0F,  9.317511909335F,  -10.764218674777F,  2.29147417383887F},
-        CubicSplineCoefficients<float>{-5.0F, 2.933359899275F,  8.4841643854695F,   -6.417524284744F },
-        CubicSplineCoefficients<float>{0.0F,  0.649115815980F,  -10.76840846876F,   8.11929265278F   },
-        CubicSplineCoefficients<float>{-2.0F, 3.47017683680F,   13.58946948958F,    -9.05964632639F  },
+        CubicSplineCoefficients<float>{
+                                       .a = 0.0F,
+                                       .b = 2.67435188089F,
+                                       .c = -2.22044604e-16F,
+                                       .d = -1.174351880F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 1.5F,
+                                       .b = -0.8487037617F,
+                                       .c = -3.523055642F,
+                                       .d = 3.17175940441F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 0.3F,
+                                       .b = 1.62046316623F,
+                                       .c = 5.992222570F,
+                                       .d = -5.11268573691F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 2.8F,
+                                       .b = -1.733148903F,
+                                       .c = -9.3458346400F,
+                                       .d = 9.37898354322F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 1.1F,
+                                       .b = 7.7121324463F,
+                                       .c = 18.791115989F,
+                                       .d = -49.2588911072F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 3.0F,
+                                       .b = 9.3175119093F,
+                                       .c = -10.76421867F,
+                                       .d = 2.291474173838F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = -5.0F,
+                                       .b = 2.9333598992F,
+                                       .c = 8.4841643854F,
+                                       .d = -6.4175242847F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = 0.0F,
+                                       .b = 0.6491158159F,
+                                       .c = -10.7684084F,
+                                       .d = 8.119292652F,
+                                       },
+        CubicSplineCoefficients<float>{
+                                       .a = -2.0F,
+                                       .b = 3.470176836F,
+                                       .c = 13.589469489F,
+                                       .d = -9.059646326F,
+                                       },
     };
 
     constexpr static std::array<float, 10> test_points{
@@ -37,11 +105,13 @@ TEST_CASE("IslSpline", "IslLinalg")
 
     const auto spline_coefficients = isl::interpolation::createCubicSpline<float>(dataX, dataY);
 
+    constexpr static auto precision = 1e-4F;
+
     for (std::size_t i = 0; i < spline_coefficients.size(); ++i) {
-        REQUIRE(std::abs(spline_coefficients[i].a - expected_coefficients[i].a) < 1e-4F);
-        REQUIRE(std::abs(spline_coefficients[i].b - expected_coefficients[i].b) < 1e-4F);
-        REQUIRE(std::abs(spline_coefficients[i].c - expected_coefficients[i].c) < 1e-4F);
-        REQUIRE(std::abs(spline_coefficients[i].d - expected_coefficients[i].d) < 1e-4F);
+        REQUIRE(std::abs(spline_coefficients.at(i).a - expected_coefficients.at(i).a) < precision);
+        REQUIRE(std::abs(spline_coefficients.at(i).b - expected_coefficients.at(i).b) < precision);
+        REQUIRE(std::abs(spline_coefficients.at(i).c - expected_coefficients.at(i).c) < precision);
+        REQUIRE(std::abs(spline_coefficients.at(i).d - expected_coefficients.at(i).d) < precision);
     }
 
     float x = 0.5F;
@@ -49,6 +119,6 @@ TEST_CASE("IslSpline", "IslLinalg")
     for (std::size_t i = 0; i < test_points.size(); ++i, x += 1.0F) {
         REQUIRE(
             std::abs(test_points[i] - countSplineValueInPoint<float>(dataX, spline_coefficients, x))
-            < 1e-4F);
+            < precision);
     }
 }
